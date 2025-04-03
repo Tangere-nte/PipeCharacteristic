65f8fb45a8eaae454b04d91c2334465921156300
Krit Tangsongcharoen <t.krit@thegang.tech>
2025-04-03 16:39:43 +0700

    Debug line char excess, type annotation, and whitespace of :

--- src/my_package/pipecharacteristic.py
+++ src/my_package/pipecharacteristic.py
@@ -1,10 +1,14 @@
-from dataclasses import dataclass, field
-from typing import Any, Dict, Tuple
+from dataclasses import dataclass
 
 import numpy as np
 
 IArray2D = np.ndarray  # Int numpy array 2D
 FArray = np.ndarray  # Float numpy array
+
+# Constant setting
+density = 700.0  # unit: kg/m^3
+gravitational_accel = 9.81  # unit: m/s^2
+k = 2.0  # empirical parameter
 
 
 @dataclass
@@ -14,18 +18,21 @@ class SummarizeResult1:
     There are 4 keys: sources, junctions, sinks, and all
     """
 
-    def __init__(self, **kwargs) -> object:
+    def __init__(self: object, **kwargs: tuple[FArray, FArray]) -> None:
         for key, (pressure_value, flow_value) in kwargs.items():
             setattr(
-                self, key, self.SubObject(pressures=pressure_value, flows=flow_value)
+                self,
+                key,
+                self.SubObject(pressures=pressure_value, flows=flow_value),
             )
 
     class SubObject:
         """
-        Suboject to split pressures and flows to .pressures and .flows respectively
+        Suboject to split pressures and flows to .pressures and
+        .flows respectively
         """
 
-        def __init__(self, **kwargs) -> object:
+        def __init__(self: object, **kwargs: tuple[FArray, FArray]) -> None:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
@@ -33,18 +40,22 @@ class SummarizeResult1:
 def calculate_flow_from_sources(
     flow_sources: FArray,  # Shape (number of sources,)
     num_vertices: int,  # the number of sources
-    edge_indices: IArray2D,  # Shape (number of edges, 2) as a pair index of source and destination
+    edge_indices: IArray2D,  # Shape (number of edges, 2)
 ) -> FArray:  # Shape (number of total nodes,)
     """
     calculate the flows of every junctions and sinks from continuity equation
-        f_j = summation fij over i; i is the init node which has the target at jth node
+        f_j = summation fij over i; i is the init node which has the target
+        at jth node
 
     Args:
-        flow_sources (FArray)   : set of the mass flow rate of each pipe in kilogram per second.
+        flow_sources (FArray)   : set of the mass flow rate of each pipe in
+                                    kilogram per second.
         num_vertices (int)      : the number of vertices (aka. nodes)
-        edge_indices (LArray2D) : indices of every init nodes and paired target of that flow,
+        edge_indices (LArray2D) : indices of every init nodes and paired target
+                                    of that flow,
                                     ordering [sources, junctions, sinks]
-                                    e.g. node 1 -> 3, and 2 -> 3, edge_indices = np.array[[1, 2], [3, 3]]
+                                    e.g. node 1 -> 3, and 2 -> 3, edge_indices
+                                    = np.array[[1, 2], [3, 3]]
 
     Returns:
         FArray  : set of each of flow rate in kilogram per second.
@@ -62,56 +73,61 @@ def calculate_flow_from_sources(
 
     # Calculate the rest flows
     for idx in node_idx_not_sources:
-        flows[idx] = (flows[edge_indices[0][np.where(edge_indices[1] == idx)]]).sum()
+        ind_target = edge_indices[0][np.where(edge_indices[1] == idx)]
+        flows[idx] = (flows[ind_target]).sum()
 
     return flows
 
 
 def calculate_p_flow(
-    edge_indices: IArray2D,  # Shape (number of edges, 2) as a pair index of source and destination
+    edge_indices: IArray2D,  # Shape (number of edges, 2)
     heights: FArray,  # Shape (number of total nodes,)
     bs: FArray,  # Shape (number of edges,)
     p_sinks: FArray,  # Shape (number of sinks,)
     flow_sources: FArray,  # Shape (number of sources)
 ) -> (
     SummarizeResult1
-):  # Shape pressures: (number of total nodes,), flows: (number of total nodes,)
+):  # Shape pressures: (number of total nodes,), flows:(number of total nodes,)
     """
-    Calculate the flows of every junctions and sinks from continuity equation
+    Calculate the flows of every junctions and sinks
+    from continuity equation
         f_j = summation fij over i
 
-    and also calculate the pressure of sources and junctions from the pressures
-    of sinks according to govern equation
+    and also calculate the pressure of sources and
+    junctions from the pressures of sinks according
+    to govern equation
         P_j = P_i - rho g (h_j - h_i) + Bij * fij ^ k
 
 
 
     Args:
-        edge_indices (LArray2D) : indices of every init nodes and pair target of that every flow
-                                    e.g. node 1 -> 3, and 2 -> 3, edge_indices = np.array[[1, 2], [3, 3]]
+        edge_indices (LArray2D) : indices of every init nodes and pair target
+                                    of that every flow
+                                    e.g. node 1 -> 3, and 2 -> 3, edge_indices
+                                    = np.array[[1, 2], [3, 3]]
         heights (FArray)        : set of the height of each node in meters.
-        bs (FArray)             : set of the pipe Characteristic of each flow in Pascal
-                                    second^k per kilogram^k.
+        bs (FArray)             : set of the pipe Characteristic of each flow
+                                    in Pascal second^k per kilogram^k.
         p_sinks (FArray)        : set of the pressure of sinks in Pascal units.
-        flow_sources (FArray)   : set of the mass flow rate of each pipe in kilogram per second.
+        flow_sources (FArray)   : set of the mass flow rate of each pipe in
+                                    kilogram per second.
 
     Constant args:
-        DENSITY (float)             : density of fluid in kilogram per cubic meters.
-        GRAVITATIONAL_ACCEL (float) : gravitational acceleration in meter per square seconds.
-        K (float)                   : constant exponential of flow to determine the
-                                        effect to friction (dimensionless variable).
+        density (float)             : density of fluid in kilogram per cubic
+                                        meters.
+        gravitational_accel (float) : gravitational acceleration in meter per
+                                        square seconds.
+        k (float)                   : constant exponential of flow to determine
+                                        the effect to friction (dimensionless
+                                        variable).
 
     Returns:
-        SummarizeResult1   : a object that collects a set of every node's pressure
-                                and a set of every pipe's flow
-                                in the unit of Pascal and kilogram per second respectively.
+        SummarizeResult1   : a object that collects a set of every node's
+                                pressure and a set of every pipe's flow
+                                in the unit of Pascal and kilogram per second
+                                respectively.
 
     """
-
-    # Constant setting
-    DENSITY = 700.0  # unit: kg/m^3
-    GRAVITATIONAL_ACCEL = 9.81  # unit: m/s^2
-    K = 2.0  # empirical parameter
 
     # Determine the numbers of sources, vertices, sinks, and edges
     num_sources = flow_sources.size
@@ -119,7 +135,11 @@ def calculate_p_flow(
     num_sinks = p_sinks.size
     num_edges = edge_indices[0].size
 
-    flows = calculate_flow_from_sources(flow_sources, num_vertices, edge_indices)
+    flows = calculate_flow_from_sources(
+        flow_sources,
+        num_vertices,
+        edge_indices,
+    )
 
     # Assign the sinks' pressure
     ps = np.zeros(num_vertices)
@@ -129,11 +149,12 @@ def calculate_p_flow(
     for edge_idx in np.arange(num_edges - 1, -1, -1):
         idx_init = edge_indices[0][edge_idx]
         idx_target = edge_indices[1][edge_idx]
+        diff_height = heights[idx_target] - heights[idx_init]
         # P_init = P_target + rho g (h_target - h_init) - Bij * fij ^ k
         ps[idx_init] = (
             ps[idx_target]
-            + DENSITY * GRAVITATIONAL_ACCEL * (heights[idx_target] - heights[idx_init])
-            - bs[edge_idx] * (flows[idx_init]) ** K
+            + density * gravitational_accel * (diff_height)
+            - bs[edge_idx] * (flows[idx_init]) ** k
         )
 
     return SummarizeResult1(
@@ -154,18 +175,18 @@ class SummarizeResult2:
     There are 4 keys: sources, junctions, sinks, and all
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self: object, **kwargs: tuple[FArray, FArray]) -> None:
         for key, b_value in kwargs.items():
             setattr(self, key, b_value)
 
 
 def calculate_pipe_characteristic(
-    edge_indices: IArray2D,  # Shape (number of edges, 2) as a pair index of init and target
+    edge_indices: IArray2D,  # Shape (number of edges, 2)
     heights: FArray,  # Shape (number of total nodes,)
     p_s: FArray,  # Shape (number of total nodes,)
     flow_sources: FArray,  # Shape (number of sources)
     num_sinks: int,  # the number of sinks
-) -> SummarizeResult2:  # Shape (number of total nodes,), (number of total nodes,)
+) -> SummarizeResult2:  # (number of total nodes,), (number of total nodes,)
     """
     Calculate the flows of every junctions and sinks from continuity equation
         f_j = summation fi,j over i
@@ -177,25 +198,29 @@ def calculate_pipe_characteristic(
 
 
     Args:
-        edge_indices (LArray2D) : indices of every init nodes and pair target of that every flow
-                                    e.g. node 1 -> 3, and 2 -> 3, edge_indices = np.array[[1, 2], [3, 3]]
+        edge_indices (LArray2D) : indices of every init nodes and pair target
+                                    of that every flow
+                                    e.g. node 1 -> 3, and 2 -> 3, edge_indices
+                                    = np.array[[1, 2], [3, 3]]
         heights (FArray)        : set of the height of each node in meters.
-        P_s (FArray)            : set of the pressure of each node in Pascal units.
-        flow_sources (FArray)   : set of the mass flow rate of each pipe in kilogram per second.
+        P_s (FArray)            : set of the pressure of each node in Pascal
+                                    units.
+        flow_sources (FArray)   : set of the mass flow rate of each pipe in
+                                    kilogram per second.
 
     Constant args:
-        density (float)             : density of fluid in kilogram per cubic meters.
-        gravitational_accel (float) : gravitational acceleration in meter per square seconds.
-        k (float)                   : constant exponential of flow to determine the effect to friction (dimensionless variable).
+        density (float)             : density of fluid in kilogram per cubic
+                                        meters.
+        gravitational_accel (float) : gravitational acceleration in meter per
+                                        square seconds.
+        k (float)                   : constant exponential of flow to determine
+                                        the effect to friction
+                                        (dimensionless variable).
 
     Returns:
-        SummarizeResult_2   : each pipe's characteristic in Pascal second per kilogram.
+        SummarizeResult_2   : each pipe's characteristic in Pascal second per
+                                kilogram.
     """
-
-    # Constant setting
-    DENSITY = 700.0  # unit: kg/m^3
-    GRAVITATIONAL_ACCEL = 9.81  # unit: m/s^2
-    K = 2.0  # empirical parameter
 
     # Determine the numbers of sources, vertices, sinks, and edges
     num_sources = flow_sources.size
@@ -210,12 +235,13 @@ def calculate_pipe_characteristic(
     for edge_idx in np.arange(num_edges):
         idx_init = edge_indices[0][edge_idx]
         idx_target = edge_indices[1][edge_idx]
+        diff_height = heights[idx_target] - heights[idx_init]
         # Bij = 1/fij ^ k (P_target - P_init - rho g (h_init - h_target)
         bs[edge_idx] = (
             p_s[idx_target]
             - p_s[idx_init]
-            - DENSITY * GRAVITATIONAL_ACCEL * (heights[idx_init] - heights[idx_target])
-        ) / (flows[idx_init]) ** K
+            - density * gravitational_accel * (diff_height)
+        ) / (flows[idx_init]) ** k
 
     return SummarizeResult2(
         sources=bs[:num_sources],
